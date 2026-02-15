a059645dbb4ea6ad19734d2184db647cbbe40808
raminhrd <raminhr.work@gmail.com>
2026-02-16 01:23:18 +0330

    update listing views

--- listing/views.py
+++ listing/views.py
@@ -1,10 +1,14 @@
-from django.db.models import Q
+from datetime import timedelta
+from django.utils import timezone
+
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from datetime import timedelta, timezone
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 from .models import Listing, ListingView
+from .filters import ListingFilter
 from .serializers import ListingListSerializer, ListingDetailSerializer
 from .permissions import IsOwnerAndEditable
 
@@ -15,75 +19,41 @@ class ListingViewSet(
     mixins.CreateModelMixin,
     mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
-    viewsets.GenericViewSet,
-    ):
-    
-    queryset = (
-        Listing.objects
-        .select_related("owner")
-        .prefetch_related("images")
-        .order_by("-created_at")
-    )
+    viewsets.GenericViewSet,):
 
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerAndEditable]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = ListingFilter
+    ordering_fields = ["created_at", "price", "year", "views_count"]
+    ordering = ["-created_at"]
 
     def get_serializer_class(self):
-        if self.action == "list":
-            return ListingListSerializer
-        return ListingDetailSerializer
+        return ListingListSerializer if self.action == "list" else ListingDetailSerializer
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        return (
+            Listing.objects
+            .select_related("owner")
+            .prefetch_related("images")
+            .with_views_count()
+        )
+
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
-
-        user = request.user if request.user.is_authenticated else None
-
-        # Owner view shouldn't count
-        if user != instance.owner:
-            twenty_four_hours_ago = timezone.now() - timedelta(hours=24)
+        if request.user.is_authenticated and request.user != instance.owner:
+            since = timezone.now() - timedelta(hours=24)
 
             already_viewed = ListingView.objects.filter(
                 listing=instance,
-                user=user,
-                created_at__gte=twenty_four_hours_ago).exists()
+                user=request.user,
+                created_at__gte=since,
+            ).exists()
 
             if not already_viewed:
-                ListingView.objects.create(
-                    listing=instance,
-                    user=user)
+                ListingView.objects.create(listing=instance, user=request.user)
 
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
-
-    def get_queryset(self):
-        qs = super().get_queryset()
-        params = self.request.query_params
-
-        brand = params.get("brand")
-        city = params.get("city")
-        min_price = params.get("min_price")
-        max_price = params.get("max_price")
-        min_year = params.get("min_year")
-        max_year = params.get("max_year")
-
-        if brand:
-            qs = qs.filter(brand__icontains=brand)
-
-        if city:
-            qs = qs.filter(city__icontains=city)
-
-        if min_price:
-            qs = qs.filter(price__gte=min_price)
-
-        if max_price:
-            qs = qs.filter(price__lte=max_price)
-
-        if min_year:
-            qs = qs.filter(year__gte=min_year)
-
-        if max_year:
-            qs = qs.filter(year__lte=max_year)
-
-        return qs
