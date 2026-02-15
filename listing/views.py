32bb5798b6db6149d0e48d431ca4d1340234b09b
raminhrd <raminhr.work@gmail.com>
2026-02-16 00:50:48 +0330

    add count view for listing

--- listing/views.py
+++ listing/views.py
@@ -1,8 +1,10 @@
 from django.db.models import Q
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from datetime import timedelta, timezone
 
-from .models import Listing
+from .models import Listing, ListingView
 from .serializers import ListingListSerializer, ListingDetailSerializer
 from .permissions import IsOwnerAndEditable
 
@@ -32,6 +34,28 @@ class ListingViewSet(
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        user = request.user if request.user.is_authenticated else None
+
+        # Owner view shouldn't count
+        if user != instance.owner:
+            twenty_four_hours_ago = timezone.now() - timedelta(hours=24)
+
+            already_viewed = ListingView.objects.filter(
+                listing=instance,
+                user=user,
+                created_at__gte=twenty_four_hours_ago).exists()
+
+            if not already_viewed:
+                ListingView.objects.create(
+                    listing=instance,
+                    user=user)
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
     def get_queryset(self):
         qs = super().get_queryset()
