e73014cc0091cebf9775bdd2f43373ad29e6df28
raminhrd <raminhr.work@gmail.com>
2026-02-16 00:14:41 +0330

    add serializer for listing 🙊

--- listing/serializers.py
+++ listing/serializers.py
@@ -1,1 +1,45 @@
+from rest_framework import serializers
+from .models import Listing
 
+
+class ListingListSerializer(serializers.ModelSerializer):
+    owner_name = serializers.CharField(source="owner.full_name", read_only=True)
+    main_image = serializers.ImageField(source="main_image_file", read_only=True)
+
+    class Meta:
+        model = Listing
+        fields = (
+            "id",
+            "title",
+            "price",
+            "city",
+            "year",
+            "mileage",
+            "main_image",
+            "owner_name",
+            "created_at",
+        )
+
+class ListingDetailSerializer(serializers.ModelSerializer):
+    owner_name = serializers.CharField(source="owner.full_name", read_only=True)
+    listing_main_image = serializers.ImageField(source="main_image_file", read_only=True)
+    images = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Listing
+        fields = (
+            "id",
+            "title",
+            "description",
+            "price",
+            "brand",
+            "model",
+            "year",
+            "mileage",
+            "city",
+            "images",
+            "owner",
+            "owner_name",
+            "created_at",
+        )
+        read_only_fields = ("owner", "created_at")
